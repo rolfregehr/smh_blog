33ef1e45cea66448ffa733df06a7bf92bc271e44
Rolf <rolf.regehr@gmail.com>
2024-10-05 11:28:47 -0300

    Modelo e 'admin' configurados

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Produto, Cliente
+
+class ProdutoAdmin(admin.ModelAdmin): # Lista, no gerenciador (/admin) os campos definidos abaixo 
+    list_display = ('nome', 'preco', 'estoque')
+
+admin.site.register(Produto, ProdutoAdmin) # tem que incluir a class criada para visualização
+admin.site.register(Cliente)
--- blog/models.py
+++ blog/models.py
@@ -5,9 +5,11 @@ class Produto(models.Model):
     nome = models.CharField('Nome', max_length=100)
     preco = models.DecimalField('Preço', decimal_places=2, max_digits=8)
     estoque = models.IntegerField('Quantidade em estoque')
+    def __str__(self):
+        return self.nome
 
 
 class Cliente(models.Model):
     nome = models.CharField('Nome', max_length=100)
-    sobrenome=models.CharField('Sobrnome', max_length=100)
+    sobrenome=models.CharField('Sobrenome', max_length=100)
     email = models.EmailField('e-mail', max_length=100)
